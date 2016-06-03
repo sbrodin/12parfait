44913e0ca7df6f185f335690cc90373281efd280
Stanislas Brodin <stanislas.brodin@globalis-ms.com>
2016-06-03 09:35:14 +0200

    Modification de la structure de base pour utf8

--- 12parfait.sql
+++ 12parfait.sql
@@ -1,39 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 4.0.8
--- http://www.phpmyadmin.net
---
--- Client: localhost
--- Généré le: Lun 30 Novembre 2015 à 10:31
--- Version du serveur: 5.1.56-log
--- Version de PHP: 5.3.27
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de données: `stanislas_veille_12parfait`
---
-
--- --------------------------------------------------------
-
 --
 -- Structure de la table `bet`
 --
 
 CREATE TABLE IF NOT EXISTS `bet` (
-  `bet_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `bet_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `match_id` int(11) NOT NULL,
   `score` varchar(255) NOT NULL,
   `result` set('1','N','2') NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`bet_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -42,14 +18,14 @@ CREATE TABLE IF NOT EXISTS `bet` (
 --
 
 CREATE TABLE IF NOT EXISTS `championship` (
-  `championship_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `championship_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `sport` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL,
   `level` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `year` year(4) NOT NULL,
   PRIMARY KEY (`championship_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -61,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `championship_team` (
   `championship_id` int(11) NOT NULL,
   `team_id` int(11) NOT NULL,
   `group` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -73,11 +49,11 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
   `id` varchar(40) NOT NULL DEFAULT '0',
   `ip_address` varchar(45) NOT NULL DEFAULT '0',
   `user_agent` varchar(120) NOT NULL,
-  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
+  `timestamp` int(11) unsigned NOT NULL DEFAULT '0',
   `data` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `last_activity_idx` (`timestamp`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -86,11 +62,11 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
 --
 
 CREATE TABLE IF NOT EXISTS `league` (
-  `league_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `league_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `hash` varchar(255) NOT NULL,
   PRIMARY KEY (`league_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -101,7 +77,7 @@ CREATE TABLE IF NOT EXISTS `league` (
 CREATE TABLE IF NOT EXISTS `league_championship` (
   `league_id` int(11) NOT NULL,
   `championship_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -110,14 +86,14 @@ CREATE TABLE IF NOT EXISTS `league_championship` (
 --
 
 CREATE TABLE IF NOT EXISTS `match` (
-  `match_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `match_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `team1_id` int(11) NOT NULL,
   `team2_id` int(11) NOT NULL,
   `score` varchar(255) DEFAULT NULL,
   `result` set('1','N','2') DEFAULT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`match_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -126,10 +102,10 @@ CREATE TABLE IF NOT EXISTS `match` (
 --
 
 CREATE TABLE IF NOT EXISTS `team` (
-  `team_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `team_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`team_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -138,7 +114,7 @@ CREATE TABLE IF NOT EXISTS `team` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `acl` set('admin','moderator','user') NOT NULL,
   `active` tinyint(1) NOT NULL,
   `first_name` varchar(255) NOT NULL,
@@ -152,7 +128,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `hash` varchar(255) DEFAULT NULL,
   `score` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -163,7 +139,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 CREATE TABLE IF NOT EXISTS `user_bet` (
   `user_id` int(11) NOT NULL,
   `bet_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -175,8 +151,4 @@ CREATE TABLE IF NOT EXISTS `user_league` (
   `user_id` int(11) NOT NULL,
   `league_id` int(11) NOT NULL,
   `role` set('leader','follower') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
