fa85e85bfe19c660f579208f0540f41fc673cb6f
Stanislas Brodin <stanislas.brodin@globalis-ms.com>
2016-06-21 20:53:02 +0200

    Header et footer de base

---
+++ application/views/templates/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+
+        <title><?php echo $title ?></title>
+        <meta name="description" content="<?php echo $title ?>">
+        <meta name="author" content="SitePoint">
+
+        <!-- <link rel="stylesheet" href="css/styles.css?v=1.0"> -->
+
+        <!--[if lt IE 9]>
+            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+        <a href="<?php echo site_url('connection/logout') ?>"><?php echo $this->lang->line('disconnection');?></a>
