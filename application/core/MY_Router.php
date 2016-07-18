89abaf1e509ca17e120da852c7cd1d0e87e038fc
Stanislas Brodin <stanislas.brodin@gmail.com>
2016-07-18 08:51:49 +0200

    Config routes

--- application/core/MY_Router.php
+++ application/core/MY_Router.php
@@ -12,48 +12,48 @@ class MY_Router extends CI_Router {
      *
      * @return  void
      */
-    protected function _set_default_controller()
-    {
+    // protected function _set_default_controller()
+    // {
 
-        if (empty($this->default_controller)) {
-            show_error('Unable to determine what should be displayed. A default route has not been specified in the routing file.');
-        }
+    //     if (empty($this->default_controller)) {
+    //         show_error('Unable to determine what should be displayed. A default route has not been specified in the routing file.');
+    //     }
 
-        $segments =  explode('/', $this->default_controller);
+    //     $segments =  explode('/', $this->default_controller);
 
-        $nb_segment = count($segments);
+    //     $nb_segment = count($segments);
 
-        //Unique segment = controller only
-        if ($nb_segment === 1) {
-            $method = 'index';
-        } else {
-            //Two last elements can be conttroller + mehtod OR dir + conttroller
-            $class = array_pop($segments);
-            $this->set_directory(implode('/', $segments));
-            if (!file_exists(APPPATH.'controllers/'.$this->directory.ucfirst($class).'.php')) {
-                $method = $class;
-                $class = array_pop($segments);
-                $this->set_directory(implode('/', $segments));
-            } else {
-                $method = 'index';
-            }
-        }
+    //     //Unique segment = controller only
+    //     if ($nb_segment === 1) {
+    //         $method = 'index';
+    //     } else {
+    //         //Two last elements can be conttroller + mehtod OR dir + conttroller
+    //         $class = array_pop($segments);
+    //         $this->set_directory(implode('/', $segments));
+    //         if (!file_exists(APPPATH.'controllers/'.$this->directory.ucfirst($class).'.php')) {
+    //             $method = $class;
+    //             $class = array_pop($segments);
+    //             $this->set_directory(implode('/', $segments));
+    //         } else {
+    //             $method = 'index';
+    //         }
+    //     }
 
-        if ( ! file_exists(APPPATH.'controllers/'.$this->directory.ucfirst($class).'.php')) {
-            // This will trigger 404 later
-            return;
-        }
+    //     if ( ! file_exists(APPPATH.'controllers/'.$this->directory.ucfirst($class).'.php')) {
+    //         // This will trigger 404 later
+    //         return;
+    //     }
 
 
-        $this->set_class($class);
-        $this->set_method($method);
+    //     $this->set_class($class);
+    //     $this->set_method($method);
 
-        // Assign routed segments, index starting from 1
-        $this->uri->rsegments = array(
-            1 => $class,
-            2 => $method
-        );
+    //     // Assign routed segments, index starting from 1
+    //     $this->uri->rsegments = array(
+    //         1 => $class,
+    //         2 => $method
+    //     );
 
-        log_message('debug', 'No URI present. Default controller set.');
-    }
+    //     log_message('debug', 'No URI present. Default controller set.');
+    // }
 }
