5fcde7d4f1b14c200bc32a9bd145cc45fe81e3f3
Stanislas Brodin <stanislas.brodin@globalis-ms.com>
2016-10-18 18:46:42 +0200

    Aspect profil + suppression choix langue

--- application/views/profile/edit.php
+++ application/views/profile/edit.php
@@ -6,25 +6,25 @@
         <tbody>
             <tr>
                 <td><label for="first_name"><?= $this->lang->line('first_name')?> : </label></td>
-                <td><input type="text" id="first_name" name="first_name" value="<?= set_value('first_name') ? set_value('first_name') : $user->first_name ; ?>"></td>
+                <td><input type="text" id="first_name" name="first_name" class="form-control" value="<?= set_value('first_name') ? set_value('first_name') : $user->first_name ; ?>"></td>
             </tr>
             <tr>
                 <td><label for="last_name"><?= $this->lang->line('last_name') ?> : </label></td>
-                <td><input type="text" id="last_name" name="last_name" value="<?= set_value('last_name') ? set_value('last_name') : $user->last_name ; ?>"></td>
+                <td><input type="text" id="last_name" name="last_name" class="form-control" value="<?= set_value('last_name') ? set_value('last_name') : $user->last_name ; ?>"></td>
             <tr>
             <tr>
                 <td><label for="user_name"><?= $this->lang->line('user_name') ?> : </label></td>
-                <td><input type="text" id="user_name" name="user_name" value="<?= set_value('user_name') ? set_value('user_name') : $user->user_name ; ?>"></td>
+                <td><input type="text" id="user_name" name="user_name" class="form-control" value="<?= set_value('user_name') ? set_value('user_name') : $user->user_name ; ?>"></td>
             <tr>
-            <tr>
+            <!-- <tr>
                 <td><label for="language"><?= $this->lang->line('language') ?> : </label></td>
                 <td>
-                    <select id="language" name="language" required="required">
+                    <select id="language" name="language" class="form-control" required="required">
                         <option value="french" <?= ($user->language == 'french') ? 'selected' : '' ?>><?= $this->lang->line('french') ?></option>
                         <option value="english" <?= ($user->language == 'english') ? 'selected' : '' ?> ><?= $this->lang->line('english') ?></option>
                     </select>
                 </td>
-            <tr>
+            <tr> -->
         </tbody>
     </table>
 
