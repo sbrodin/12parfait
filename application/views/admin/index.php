8625a246afc9943663e867f6d3d5abfaaa0e90cf
Stanislas Brodin <stanislas.brodin@globalis-ms.com>
2016-11-02 16:26:40 +0100

    Aspect index admin

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,6 +1,6 @@
-<a href="<?= site_url('admin/fixtures') ?>"><?= $this->lang->line('fixtures_admin');?></a><br/>
-<a href="<?= site_url('admin/messages') ?>"><?= $this->lang->line('messages_admin');?></a><br/>
-<a href="<?= site_url('admin/users') ?>"><?= $this->lang->line('users_admin');?></a><br/>
-<a href="<?= site_url('admin/championships') ?>"><?= $this->lang->line('championships_admin');?></a><br/>
-<a href="<?= site_url('admin/teams') ?>"><?= $this->lang->line('teams_admin');?></a><br/>
-<a href="<?= site_url('admin/matches') ?>"><?= $this->lang->line('matches_admin');?></a><br/>
+<a href="<?= site_url('admin/fixtures') ?>" class="btn btn-sm btn-outline-primary m-b-1"><?= $this->lang->line('fixtures_admin');?></a><br/>
+<a href="<?= site_url('admin/messages') ?>" class="btn btn-sm btn-outline-primary m-b-1"><?= $this->lang->line('messages_admin');?></a><br/>
+<a href="<?= site_url('admin/users') ?>" class="btn btn-sm btn-outline-primary m-b-1"><?= $this->lang->line('users_admin');?></a><br/>
+<a href="<?= site_url('admin/championships') ?>" class="btn btn-sm btn-outline-primary m-b-1"><?= $this->lang->line('championships_admin');?></a><br/>
+<a href="<?= site_url('admin/teams') ?>" class="btn btn-sm btn-outline-primary m-b-1"><?= $this->lang->line('teams_admin');?></a><br/>
+<a href="<?= site_url('admin/matches') ?>" class="btn btn-sm btn-outline-primary m-b-1"><?= $this->lang->line('matches_admin');?></a><br/>
