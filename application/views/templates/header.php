e9504d7496323755a364a870337cfeecfc07a490
Stanislas Brodin <stanislas.brodin@gmail.com>
2018-02-20 15:28:25 +0100

    Ajout de la couleur de theme

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,6 +9,7 @@
         <meta name="author" content="Stanislas Brodin">
         <meta name="keywords" content="site, 12parfait, paris, bets, amis, friends, football, 12, parfait, fun, sport, ligue1, ligue 1, coupe du monde, coupedumonde, world cup, worldcup">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="theme-color" content="#0275d8">
 
         <link href="<?= img_url('logo.png') ?>" rel="icon" type="image/png">
         <link href="<?= img_url('logo.png') ?>" rel="apple-touch-icon">
