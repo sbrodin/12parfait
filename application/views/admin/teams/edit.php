765dbc5eebfb4d623dc6fc09b985c59125e7a4af
Stanislas Brodin <stanislas.brodin@gmail.com>
2018-03-22 15:32:36 +0100

    Amélioration de l'affichage des formulaires des équipes

--- application/views/admin/teams/edit.php
+++ application/views/admin/teams/edit.php
@@ -2,9 +2,20 @@
 <?= validation_errors(); ?>
 
 <?= form_open('admin/teams/edit/'.$team->team_id); ?>
-    <label for="team_name"><?= $this->lang->line('team_name') ?> : </label>
-    <input type="text" id="team_name" name="team_name" value="<?= $team->name ?>" required="required" autofocus>
-    <label for="team_short_name"><?= $this->lang->line('team_short_name') ?> : </label>
-    <input type="text" id="team_short_name" name="team_short_name" value="<?= $team->short_name ?>" required="required" autofocus>
+    <div>
+        <label for="team_name"><?= $this->lang->line('team_name') ?> : </label>
+        <input type="text" id="team_name" name="team_name" value="<?= $team->name ?>" required="required" autofocus>
+    </div>
+    <div>
+        <label for="team_short_name"><?= $this->lang->line('team_short_name') ?> : </label>
+        <input type="text" id="team_short_name" name="team_short_name" value="<?= $team->short_name ?>" required="required"  max_length="5">
+    </div>
+    <div>
+        <label for="level"><?= $this->lang->line('level') ?> : </label>
+        <select id="level" name="level" required="required">
+            <option value="local"><?= $this->lang->line('local') ?></option>
+            <option value="national"><?= $this->lang->line('national') ?></option>
+        </select>
+    </div>
     <input type="submit" value="<?= $this->lang->line('confirm') ?>">
 </form>
