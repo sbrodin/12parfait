26923b3997c26c43f45c12d18da1c3e68cc54f70
Stanislas Brodin <stanislas.brodin@gmail.com>
2018-03-26 10:09:09 +0200

    Modification des logs pour MY_Controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -12,10 +12,21 @@ class MY_Controller extends CI_Controller {
         parent::__construct();
         $this->load->model('user_model');
 
+        // Authentification de l'utilisateur
+        if (!is_connected()) {
+            $this->load->model('log_model');
+            save_log('my_controller', '__construct', 'utilisateur non connecté,<br/> tentative d\'accès à l\'url : ' . (isset($_SERVER['HTTPS']) ? 'https' : 'http') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+            // Redirige l'utilisateur vers la page de connexion s'il n'est pas authentifié
+            $this->lang->load('12parfait', $this->config->item('language'));
+            redirect(site_url(), 'location');
+        } else {
+            $this->lang->load('12parfait', $this->session->user->language);
+        }
+
         // Si l'utilisateur n'est plus actif, il doit être déconnecté
         if (!$this->user_model->is_active()) {
             $this->load->model('log_model');
-            save_log('my_controller', '__construct', 'utilisateur désactivé');
+            save_log('my_controller', '__construct', 'utilisateur désactivé, url : ' . (isset($_SERVER['HTTPS']) ? 'https' : 'http') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
             if (!empty($this->session->userdata('user'))) {
                 $this->session->unset_userdata('user');
             }
@@ -25,15 +36,6 @@ class MY_Controller extends CI_Controller {
             delete_cookie('ci_session');
             redirect(site_url(''), 'location');
             exit;
-        }
-
-        // Authentification de l'utilisateur
-        if (!is_connected()) {
-            // Redirige l'utilisateur vers la page de connexion s'il n'est pas authentifié
-            $this->lang->load('12parfait', $this->config->item('language'));
-            redirect(site_url(), 'location');
-        } else {
-            $this->lang->load('12parfait', $this->session->user->language);
         }
 
         // on n'active le profiler qu'en dev
