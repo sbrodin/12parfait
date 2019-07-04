563fe00b7419c602e2700c438c8776eea00b329d
Stanislas Brodin <sbrodin@silamir.com>
2019-07-04 11:20:19 +0200

    Ajout du lien vers logomakr + Suppression de lignes inutiles

--- application/views/terms.php
+++ application/views/terms.php
@@ -17,13 +17,16 @@
     <p>Stanislas Brodin</p>
 </p>
 <hr/>
-<p>
+<!-- <p>
     L'accès au site <strong>https://12parfait.fr</strong> ainsi que l'utilisation de son contenu sont soumis aux conditions d'utilisation décrites ci-après. Le fait d'accéder et de naviguer sur le site <strong>https://12parfait.fr</strong> constitue de la part du visiteur une acceptation sans réserve des stipulations suivantes :
-</p>
+</p> -->
+<p>Created my free logo at <a href="https://logomakr.com/" rel="noopener">LogoMakr.com</a></p>
+<hr/>
 <h4>Propriété intellectuelle</h4>
 <p>
     <p>Le site est la propriété exclusive de Stanislas Brodin, seul habilité à utiliser et exploiter les droits de propriété intellectuelle et droits de la personnalité attachés à ce site, notamment les marques, dessins et modèles, droits d'auteur et droit à l'image, à titre originaire ou par l'effet d'une cession ou d'une licence d'exploitation.</p>
     <p>L'usage de tout ou partie du site <strong>https://12parfait.fr</strong>, notamment par téléchargement, reproduction, transmission ou représentation à d'autres fins que pour usage personnel et privé dans un but non commercial est strictement interdit.</p>
 </p>
+<hr/>
 
 <h3 id="terms_thanks">Merci d'utiliser 12parfait !</h3>
