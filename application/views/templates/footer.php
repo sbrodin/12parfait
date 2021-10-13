14d328d983cc9ee31a7c03207c1e97405fc23935
Stanislas Brodin <stanislas.brodin@actudata.fr>
2021-10-13 18:31:22 +0400

    Updated footer for small screen

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,10 +2,10 @@
             </div>
         </div>
         <footer>
-            <span class="m-r-2"><a href="<?= site_url('rules') ?>"><?= $this->lang->line('rules') ?></a></span>
-            <span class="m-r-2"><a href="https://stanislas-brodin.fr" rel="author"><?= $this->lang->line('copyright') ?></a></span>
-            <span class="m-r-2"><?= $this->lang->line('generated_with') ?></span>
-            <span class="m-r-2"><a href="<?= site_url('terms') ?>"><?= $this->lang->line('terms_of_use') ?></a></span>
+            <span><a href="<?= site_url('rules') ?>"><?= $this->lang->line('rules') ?></a></span>
+            <span><a href="https://stanislas-brodin.fr" rel="author"><?= $this->lang->line('copyright') ?></a></span>
+            <span><?= $this->lang->line('generated_with') ?></span>
+            <span><a href="<?= site_url('terms') ?>"><?= $this->lang->line('terms_of_use') ?></a></span>
             <span><a href="<?= site_url('donate') ?>"><?= $this->lang->line('donate') ?></a></span>
             <!-- <span><?= $this->lang->line('version') ?></span> -->
         </footer>
